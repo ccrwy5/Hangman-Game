8590d85e3f2d618efee412ccc26d72b6beffa9f9
Chris Rehagen <ccrwy5@mail.missouri.edu>
2020-08-10 12:26:58 -0500

    error handling for duplicate letters

--- src/App.js
+++ src/App.js
@@ -1,56 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 import Header from "./components/Header";
 import Figure from "./components/Figure";
 import WrongLetters from "./components/WrongLetters";
 import Word from "./components/Word";
+import Notification from "./components/Notification";
+import Popup from "./components/Popup";
 
-const words = ['application', 'programming', 'interface', 'wizard']
+import { showNotification as show } from "./helpers/helpers";
+
+const words = ["application", "programming", "interface", "wizard"];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 function App() {
-
   const [playable, setPlayable] = useState(true);
-  const [correctLetters, setCorrectLetters] = useState([])
-  const [wrongLetters, setWrongLetters] = useState([])
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
 
   useEffect(() => {
-    const handleKeydown = event => {
+    const handleKeydown = (event) => {
       const { key, keyCode } = event;
-      if(playable && keyCode >= 65 && keyCode <= 90) {
+      if (playable && keyCode >= 65 && keyCode <= 90) {
         const letter = key.toLowerCase();
 
-        if(selectedWord.includes(letter)) {
-          if(!correctLetters.includes(letter)) {
-            setCorrectLetters(currentLetters => [...currentLetters, letter]);
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters((currentLetters) => [...currentLetters, letter]);
           } else {
-            // showNotification();
+            show(setShowNotification);
           }
         } else {
-          if(!wrongLetters.includes(letter)){
-            setWrongLetters(wrongLetters => [...wrongLetters, letter]);
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters((wrongLetters) => [...wrongLetters, letter]);
+          } else {
+            show(setShowNotification);
           }
         }
       }
-    }
+    };
 
-    window.addEventListener('keydown', handleKeydown);
+    window.addEventListener("keydown", handleKeydown);
 
-    return () => window.removeEventListener('keydown', handleKeydown);
+    return () => window.removeEventListener("keydown", handleKeydown);
   }, [correctLetters, wrongLetters, playable]);
-
 
   return (
     <>
       <Header />
       <div className="game-container">
-        <Figure wrongLetters={wrongLetters}/>
+        <Figure wrongLetters={wrongLetters} />
         <WrongLetters wrongLetters={wrongLetters} />
-        <Word selectedWord={selectedWord} correctLetters={correctLetters}/>
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
       </div>
-      
+      <Popup />
+      <Notification showNotification={showNotification} />
     </>
   );
 }
